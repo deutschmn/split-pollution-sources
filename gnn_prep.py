a442f11067e6268ba3adc0a47db484d1a037a48d
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2021-04-18 16:10:13 +0200

    Add support for plotting R_out

--- gnn_prep.py
+++ gnn_prep.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 def transform_preds_back(time_npy, pred_npy, devices, data_start, melted=True):
     parsed_timestamps = pd.DataFrame(time_npy).applymap(pd.Timestamp.fromtimestamp)
@@ -14,9 +13,9 @@ def transform_preds_back(time_npy, pred_npy, devices, data_start, melted=True):
     else:
         return pred_df
 
-def transform_R_back(time_npy, R_npy, devices, data_start, prefix=""):
+def transform_R_back(time_npy, R_npy, devices, data_start):
     if R_npy.shape[0] == 0:
-        return None
+        return None, None
 
     parsed_timestamps = pd.DataFrame(time_npy).applymap(pd.Timestamp.fromtimestamp)
     # TODO find out whether off-by-one is still there
@@ -27,8 +26,10 @@ def transform_R_back(time_npy, R_npy, devices, data_start, prefix=""):
     # -> use only the last preds and don't look at the history
     pred_index = pred_index[-R_npy.shape[1]:]
 
-    R_dict = {}
+    R_in = {}
+    R_out = {}
     for device_idx, device_id in enumerate(devices):
-        R_dict[device_id] = pd.DataFrame(R_npy[date_idx, :, device_idx, :], index=pred_index, columns=devices)
+        R_in[device_id] = pd.DataFrame(R_npy[date_idx, :, device_idx, :], index=pred_index, columns=devices)
+        R_out[device_id] = pd.DataFrame(R_npy[date_idx, :, :, device_idx], index=pred_index, columns=devices)
 
-    return R_dict
+    return R_in, R_out
