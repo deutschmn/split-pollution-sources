1c8b0bf77194edc09f9458ec7afbd7e31eb3ae90
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2021-04-27 11:29:30 +0200

    Move experiment out into separate file

--- gnn_prep.py
+++ gnn_prep.py
@@ -1,6 +1,34 @@
 import pandas as pd
 import numpy as np
 
+class Experiment:
+    def __init__(self, name, path, start_date, devices):
+        self.name = name
+        self.path = path
+        self.start_date = start_date
+        self.devices = devices
+        self.preds, self.R_in, self.R_out, self.R_npy = self.load_experiment()
+
+    def load_experiment(self):
+        time_npy = np.load(self.path + 'time.npy')
+        pred_npy = np.load(self.path + 'predict.npy')
+
+        preds = transform_preds_back(time_npy, pred_npy, self.devices, self.start_date, melted=True)
+
+        try:
+            R_npy = np.load(self.path + 'R.npy')
+            R_in, R_out = transform_R_back(time_npy, R_npy, self.devices, self.start_date)
+        except FileNotFoundError as e:
+            print(f"Couldn't load R.npy for '{self.name}': {e}")
+            R_in, R_out, R_npy = None, None, None
+
+        preds['time'] = preds.index
+        return preds, R_in, R_out, R_npy
+
+    def __repr__(self):
+        return f"Experiment '{self.name}'"
+        
+
 def transform_preds_back(time_npy, pred_npy, devices, data_start, melted=True):
     parsed_timestamps = pd.DataFrame(time_npy).applymap(pd.Timestamp.fromtimestamp)
     idx = np.where(parsed_timestamps[0] == data_start)[0][0] + 1 # there is an off-by-one error somewhere
