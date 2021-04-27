f513a63b28d44ae5f67054e6f9dace75ceb984b6
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2021-04-27 21:19:24 +0200

    Add support for deep ensembles

--- ensemble.py
+++ ensemble.py
@@ -1,9 +1,26 @@
+import numpy as np
 from gnn_prep import Experiment
 from functools import reduce
+import gnn_prep
 
 def ensemblify(experiments, name):
     template_exp = list(experiments.values())[0]
     ensemble_preds = template_exp.preds.copy()
 
     ensemble_preds['pm25'] = reduce(lambda x, y: x + y, map(lambda x: x.preds['pm25'], experiments.values())) / len(experiments)
-    return Experiment(name, template_exp.start_date, template_exp.devices, ensemble_preds)
+    return Experiment(name, template_exp.start_date, template_exp.devices, ensemble_preds, None, template_exp.time_npy, template_exp.date_idx)
+
+def deep_ensemblify(experiments, name):
+    template_exp = list(experiments.values())[0]
+    date_idx, parsed_timestamps = gnn_prep.get_date_idx(template_exp.time_npy, template_exp.start_date)
+
+    R_s = np.stack(list(map(lambda exp: exp.R_npy[exp.date_idx], experiments.values())))
+    h_s = np.stack(list(map(lambda exp: exp.transform_h_back(), experiments.values())))
+    R_ens = np.average(R_s, axis=0)
+    h_ens = np.average(h_s, axis=0)
+
+    pred_npy = R_ens @ h_ens
+    preds = gnn_prep.transform_preds_back(parsed_timestamps, pred_npy, template_exp.devices, template_exp.start_date)
+    R_in, R_out = gnn_prep.transform_R_back(parsed_timestamps, R_ens, template_exp.devices, template_exp.start_date)
+    preds['time'] = preds.index
+    return Experiment(name, template_exp.start_date, template_exp.devices, preds, pred_npy, template_exp.time_npy, template_exp.date_idx, R_in, R_out, None)
--- gnn_prep.py
+++ gnn_prep.py
@@ -2,11 +2,14 @@ import pandas as pd
 import numpy as np
 
 class Experiment:
-    def __init__(self, name, start_date, devices, preds, R_in=None, R_out=None, R_npy=None):
+    def __init__(self, name, start_date, devices, preds, pred_npy, time_npy, date_idx, R_in=None, R_out=None, R_npy=None):
         self.name = name
         self.start_date = start_date
         self.devices = devices
         self.preds = preds
+        self.pred_npy = pred_npy
+        self.time_npy = time_npy
+        self.date_idx = date_idx
         self.R_in = R_in
         self.R_out = R_out
         self.R_npy = R_npy
@@ -16,52 +19,61 @@ class Experiment:
         time_npy = np.load(path + 'time.npy')
         pred_npy = np.load(path + 'predict.npy')
 
-        preds = transform_preds_back(time_npy, pred_npy, devices, start_date, melted=True)
+        date_idx, parsed_timestamps = get_date_idx(time_npy, start_date)
+        preds = transform_preds_back(parsed_timestamps, pred_npy[date_idx], devices, start_date, melted=True)
 
         try:
             R_npy = np.load(path + 'R.npy')
-            R_in, R_out = transform_R_back(time_npy, R_npy, devices, start_date)
+            R_in, R_out = transform_R_back(parsed_timestamps, R_npy[date_idx], devices, start_date)
         except FileNotFoundError as e:
             print(f"Couldn't load R.npy for '{name}': {e}")
             R_in, R_out, R_npy = None, None, None
 
         preds['time'] = preds.index
-        return cls(name, start_date, devices, preds, R_in, R_out, R_npy)
+        return cls(name, start_date, devices, preds, pred_npy, time_npy, date_idx, R_in, R_out, R_npy)
+
+    def transform_h_back(self):
+        c = self.pred_npy[self.date_idx]
+        R = self.R_npy[self.date_idx]
+        h = np.linalg.inv(R) @ c
+        return h
 
     def __repr__(self):
         return f"Experiment '{self.name}'"
 
 
-
-def transform_preds_back(time_npy, pred_npy, devices, data_start, melted=True):
+def get_date_idx(time_npy, data_start):
     parsed_timestamps = pd.DataFrame(time_npy).applymap(pd.Timestamp.fromtimestamp)
-    idx = np.where(parsed_timestamps[0] == data_start)[0][0] + 1 # there is an off-by-one error somewhere
+    # TODO find out whether off-by-one is still there
+    date_idx = np.where(parsed_timestamps[0] == data_start)[0][0] + 1 # there is an off-by-one error somewhere
+    return date_idx, parsed_timestamps
 
+
+def transform_preds_back(parsed_timestamps, pred_npy, devices, data_start, melted=True):
     pred_index = pd.Index(parsed_timestamps[parsed_timestamps[0] == data_start].iloc[0], name='time')
-    pred_df = pd.DataFrame(pred_npy[idx, :, :, 0], index=pred_index, columns=devices)
+    pred_df = pd.DataFrame(pred_npy[:, :, 0], index=pred_index, columns=devices)
 
     if melted:
         return pred_df.melt(ignore_index=False, var_name='device_id', value_name='pm25')
     else:
         return pred_df
 
-def transform_R_back(time_npy, R_npy, devices, data_start):
+
+def transform_R_back(parsed_timestamps, R_npy, devices, data_start):
     if R_npy.shape[0] == 0:
         return None, None
 
-    parsed_timestamps = pd.DataFrame(time_npy).applymap(pd.Timestamp.fromtimestamp)
-    # TODO find out whether off-by-one is still there
-    date_idx = np.where(parsed_timestamps[0] == data_start)[0][0] + 1 # there is an off-by-one error somewhere
     pred_index = pd.Index(parsed_timestamps[parsed_timestamps[0] == data_start].iloc[0], name='time')
 
     # if we use PM2.5 history, we have no R for the history
     # -> use only the last preds and don't look at the history
-    pred_index = pred_index[-R_npy.shape[1]:]
+    pred_index = pred_index[-R_npy.shape[0]:]
 
     R_in = {}
     R_out = {}
     for device_idx, device_id in enumerate(devices):
-        R_in[device_id] = pd.DataFrame(R_npy[date_idx, :, device_idx, :], index=pred_index, columns=devices)
-        R_out[device_id] = pd.DataFrame(R_npy[date_idx, :, :, device_idx], index=pred_index, columns=devices)
+        R_in[device_id] = pd.DataFrame(R_npy[:, device_idx, :], index=pred_index, columns=devices)
+        R_out[device_id] = pd.DataFrame(R_npy[:, :, device_idx], index=pred_index, columns=devices)
 
     return R_in, R_out
+
